cd560d0b7ed2f911227454105347e7d51a3db191
Pedro Chávez Hernández <pchavez28@alumnos.uaq.mx>
2023-09-10 15:08:25 -0600

    Actividad 5

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log('hola mundo')
+const express = require('express');
+const app = express();
+const { pokemon } = require('./pokedex.json')
+app.get("/", (req, res, next) => {
+    res.status(200);
+    res.send("Bienvenido al pokedex ☆*: .｡. o(≧▽≦)o .｡.:*☆");
+});
+
+app.get("/pokemon/all", (req, res, next) => {
+    res.status(200);
+    res.send(pokemon)
+});
+
+app.get('/pokemon/:id([0-9]{1,3})', (req, res, next)=>{
+    const id = req.params.id - 1;
+    if(id >= 0 && id <= 151) {
+        res.status(200);
+        return res.send(pokemon[req.params.id -1]);
+    }
+        res.status(404);
+        res.send("Pokemón no encontrado")
+});
+
+app.get('/pokemon/:name', (req, res, next) => {
+    const name = req.params.name;
+    for(i = 0; i < pokemon.length; i++) {
+        if(pokemon[i].name === name){
+            res.status(200);
+            res.send(pokemon[i]);
+        }
+    }
+    res.status(404);
+    res.send("Pokemon no encontrado");
+})
+
+app.listen(process.env.PORT || 3000, () =>{
+    console.log("Server is running...")
+});
