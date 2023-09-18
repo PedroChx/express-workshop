da320fef14e6123fd6f6a59b8fe2f9a9accf97d9
Pedro Chávez Hernández <pchavez28@alumnos.uaq.mx>
2023-09-17 20:23:41 -0600

    Tarea 6

--- index.js
+++ index.js
@@ -8,38 +8,40 @@ const { pokemon } = require('./pokedex.json')
 * POST - almacenar/crear recursos
 * PUT - modificar una parte de un recurso
 * PUT - modificar un recurso completo
+* DELETE - borrar un recurso
 * */
 
 app.get("/", (req, res, next) => {
-    res.status(200);
-    res.send("Bienvenido al pokedex ☆*: .｡. o(≧▽≦)o .｡.:*☆");
+    return res.status(200).send("Bienvenido al pokedex ☆*: .｡. o(≧▽≦)o .｡.:*☆");
 });
 
-app.get("/pokemon/all", (req, res, next) => {
-    res.status(200);
-    res.send(pokemon)
+app.get("/pokemon", (req, res, next) => {
+    return res.status(200).send(pokemon)
 });
 
 app.get('/pokemon/:id([0-9]{1,3})', (req, res, next)=>{
     const id = req.params.id - 1;
-    if(id >= 0 && id <= 151) {
-        res.status(200);
-        return res.send(pokemon[req.params.id -1]);
-    }
-        res.status(404);
-        res.send("Pokemón no encontrado")
+    (id >= 0 && id <= 151) ?
+        res.status(200).send(pokemon[req.params.id -1]) :
+        res.status(404).send("Pokemón no encontrado");
+
 });
-app.get('/pokemon/:name', (req, res, next) => {
+app.get('/pokemon/:name([A-Za-z]+)', (req, res, next) => {
+
+    //condición ? valor sí verdadero : valor si falso
+
     const name = req.params.name;
-    for(i = 0; i < pokemon.length; i++) {
-        if(pokemon[i].name === name){
-            res.status(200);
-            res.send(pokemon[i]);
-        }
-    }
-    res.status(404);
-    res.send("Pokemon no encontrado");
-})
+    const pk = pokemon.filter((p)=> {
+       return (p.name.toUpperCase() === name.toUpperCase()) ? p : null;
+    });
+
+    console.log(pk);
+
+    (pk.length > 0) ?
+        res.status(200).send(pk) : 
+        res.status(404).send("Pokemon no encontrado");
+
+});
 
 app.listen(process.env.PORT || 3000, () =>{
     console.log("Server is running...")
